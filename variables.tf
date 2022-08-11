80a6735a166c8a96a0ff3c510c4eb68b94e73bf2
Krishan Shamod <krishanshamod@gmail.com>
2022-08-11 22:20:56 +0530

    Updated the variables file

--- variables.tf
+++ variables.tf
@@ -8,20 +8,20 @@ variable "location" {
 
 variable "aks_service_principal_client_id" {
   type    = string
-  default = "564f517d-0302-4ae8-88ae-eeb4e7c7dfe6"
+  default = "value"
 }
 
 variable "aks_service_principal_client_secret" {
   type    = string
-  default = "pcY8Q~QUNEogluvCfoleIc4IG_m1Iir5o8e3DaGM"
+  default = "value"
 }
 
 variable "bastion_admin_username" {
   type    = string
-  default = "bastion"
+  default = "value"
 }
 
 variable "bastion_admin_password" {
   type    = string
-  default = "Bastion123@Com"
+  default = "value"
 }
